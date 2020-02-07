9557404d36332d629c72142e65b32d2002a5b692
Matt Applegate <mattapplegate@Matts-MacBook-Air.local>
2020-02-07 07:48:26 -0500

    Update scripts.js console.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
-console.log("scripts.js has loaded!");
+let userInput;
+userInput = "samosa";
+if ( userInput === "burrito" ) {
+$("response").html("Brilliant  choice!");
+} else {
+  $("response").html("Don't you want a burrito?");
+}
