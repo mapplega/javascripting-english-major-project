179672e12485706b7ac80add7f6d863d28c38ded
Matt Applegate <mattapplegate@Matts-MacBook-Air.local>
2020-02-12 13:58:47 -0500

    tip calculator using functions

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+/*
 let i;
 i = 1;
 while ( i < 2 ) {
@@ -12,3 +13,24 @@ while ( i === 3 ) {
   $("#response").append("<br />" + i + " is odd ");
   i = i + 2;
 }
+*/
+/*
+for (let i = 1; i <= 4; i = i + 1) {
+  $("#response").append("<br />" + i);
+}
+*/
+/*let userInput;
+userInput = prompt("Pick a number.", "Type your answer here.");
+for (let i = 1; i <= userInput; i = i + 1)
+if ( i % 2 == 0) {
+  $("#response").append("<br />" + i + " is even.");
+} else {
+  $("#response").append("<br />" + i + " is odd.")
+};
+*/
+let tipCalculator;
+tipCalculator = function(total, tipRtate){
+  tipAmount = tipRate * total;
+  $("#response").html("Your tip is $" + tipAmount);
+};
+tipCalculator(50.00, 0.2);
