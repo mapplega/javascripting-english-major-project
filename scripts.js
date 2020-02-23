a4eab00217495ebfc3cf3adc56938eae89999846
Matt Applegate <mattapplegate@Matts-MacBook-Air.local>
2020-02-23 09:08:38 -0500

    added functions exercise answer

--- scripts.js
+++ scripts.js
@@ -28,9 +28,53 @@ if ( i % 2 == 0) {
   $("#response").append("<br />" + i + " is odd.")
 };
 */
-let tipCalculator;
-tipCalculator = function(total, tipRtate){
-  tipAmount = tipRate * total;
-  $("#response").html("Your tip is $" + tipAmount);
+/*let makeABurrito;
+makeABurrito = function(beansVariable){
+  let beansResponse;
+  beansResponse = "You ordered " + beansVariable + " beans. Good choice!";
+  $("#response").html(beansResponse);
+};
+let blackBeans;
+blackBeans = "black";
+makeABurrito(blackBeans);
+*/
+/*let tipCalculator;
+tipCalculator = function(tipVariable){
+  // step 1:
+  let tipResponse;
+  tipResponse = "Your tip is $" + tipVariable;
+  // and step 2:
+  $("#response").html(tipResponse);
+};
+let tipAmount;
+tipAmount = 50 % 20;
+tipCalculator(tipAmount);
+*/
+/*let tipCalculator;
+tipCalculator = function(total, tipRate){
+  let tipAmount;
+  tipAmount = function(tipRate){
+    if (tipRate < 1) {
+      $("#response").html("Your tip is $" + tipAmount * total);
+  } else {
+    $("#response").html("Your tip is $" + tipAmount % total);}
+  };
 };
 tipCalculator(50.00, 0.2);
+*/
+let tipCalculator;
+tipCalculator = function(multiVariable, divVariable){
+  let multiTotal;
+  let divTotal;
+  if (tipAmount < 1){
+  multiTotal = total * tipAmount;
+  $("#response").html("Your tip is $" + multiTotal);} else {
+  divTotal = total % tipAmount;
+  $("#response").html("Your tip is $" + divTotal);
+}
+};
+let tipAmount;
+tipAmount = 0.2;
+let total;
+total = 50;
+tipCalculator();
